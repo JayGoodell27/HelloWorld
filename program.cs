9b5f87c08b07d6b87d5b51219e2c5db871ce1e22
JayGoodell27 <oreogoober@gmail.com>
2023-04-21 10:19:46 -0600

    wrote hello world, commented other that could be fun

---
+++ program.cs
@@ -0,0 +1,9 @@
+
+
+Console.WriteLine("Hello World");
+
+/*Console.WriteLine("What is your firtst name?");
+string? fname = Console.ReadLine();
+Console.WriteLine("What is your last name?");
+string? lname = Console.ReadLine();
+Console.WriteLine($"Hello {fname} {lname}.");*/
